4e1c9569f6d5327446c2d18755ee2163a6dfd0cf
Bryan-42 <68830449+Bryan-42@users.noreply.github.com>
2021-07-02 11:22:25 -0400

    Update Python.py

--- Python.py
+++ Python.py
@@ -2,17 +2,21 @@ import random
 import statistics
 import pandas as pd
 import plotly.figure_factory as ff
+import plotly.graph_objects as go
 
-data = pd.read_csv("medium_data.csv")
+df = pd.read_csv("medium_data.csv")
+data = df["reading_time"].tolist()
 
 def random_set_of_mean(counter):
     dataset = []
     for i in range(0,counter):
-        random_index = random.randint(0,len(data))
+        random_index = random.randint(0,len(data)-1)
         value = data[random_index]
         dataset.append(value)
     mean = statistics.mean(dataset)
     return mean
+
+    population_mean = mean
 
 def setup():
     mean_list = []
@@ -23,5 +27,10 @@ def setup():
 
 def show_fig(mean_list):
     df = mean_list
-    fig = ff.create_distplot([df], ["temp"], show_hist = False)
+    fig = ff.create_distplot([df], ["reading_time"], show_hist = False)
     fig.show()
+    std_deviation = statistics.stdev(mean_list)
+    sample_mean = statistics.mean(mean_list)
+    print(std_deviation, sample_mean)
+
+setup()
